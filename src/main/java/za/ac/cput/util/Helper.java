3f21a3b91e5e17ddbb81b13df11878079d104e21
veronica <222914556@mycput.ac.za>
2025-03-25 22:54:41 +0200

    update

--- src/main/java/za/ac/cput/util/Helper.java
+++ src/main/java/za/ac/cput/util/Helper.java
@@ -1,5 +1,14 @@
 package za.ac.cput.util;
 
 public class Helper {
-
+    public static boolean isNullOrEmpty(String s) {
+        if(s.isEmpty() || s == null)
+            return true;
+        return false;
+    }
+    public static boolean isValidEmail(String email) {
+        //TODO: Complete
+        throw  new UnsupportedOperationException();
+        //check dateOfBirth and identification
+    }
 }
