39401b0da18e0de07b4c19380bbda62111ebd2e4
220094896 <220094896@mycput.ac.za>
2025-03-27 23:49:24 +0200

    Driver

--- src/main/java/za/ac/cput/factory/DriverFactory.java
+++ src/main/java/za/ac/cput/factory/DriverFactory.java
@@ -1,23 +1,4 @@
 package za.ac.cput.factory;
 
-import za.ac.cput.domain.Driver;
-
 public class DriverFactory {
-    public static Driver createDriver(String name, String identification, String email,
-                                      String vehicleDetails, String availability, String licenseNumber) {
-        if (name == null || name.isEmpty() || identification == null || identification.isEmpty() ||
-                email == null || email.isEmpty() || vehicleDetails == null || vehicleDetails.isEmpty() ||
-                availability == null || availability.isEmpty() || licenseNumber == null || licenseNumber.isEmpty()) {
-            return null;
-        }
-
-        return new Driver.Builder()
-                .setName(name)
-                .setIdentification(identification)
-                .setEmail(email)
-                .setVehicleDetails(vehicleDetails)
-                .setAvailability(availability)
-                .setLicenseNumber(licenseNumber)
-                .build();
-    }
 }
