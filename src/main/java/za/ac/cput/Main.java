3990e5e61e68ffc0701dde942de089bd40a60bf1
veronica <222914556@mycput.ac.za>
2025-03-28 16:07:41 +0200

    update

--- src/main/java/za/ac/cput/Main.java
+++ src/main/java/za/ac/cput/Main.java
@@ -1,6 +1,26 @@
 package za.ac.cput;
 
+import za.ac.cput.domain.Address;
+import za.ac.cput.domain.Nanny;
+
 public class Main {
 
+        public static void main(String[] args) {
+            Address address = new Address("123 Main St", "Cape Town", "8001");
 
-}
+            Nanny nanny = new Nanny.Builder()
+                    .setName("Anna Smith")
+                    .setIdentification("ID123456")
+                    .setEmail("anna.smith@example.com")
+                    .setPhoneNumber("012-345-6789")
+                    .setYearsOfExperience(10)
+                    .setAvailability("Full-time")
+                    .setHourlyRate(20.50)
+                    .setAddress(address)
+                    .build();
+
+            System.out.println(nanny.toString());
+        }
+    }
+
+
